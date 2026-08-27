efebe522c6a5823cee11c56b8afee4389eb44217
Kanishka Gurav <201110976+KANISHKA-GURAV8@users.noreply.github.com>
2026-08-27 23:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (17.88%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        i=0
+        j=num
+        while i<=j:
+            mid=(i+j)//2
+            sq=mid*mid
+            if sq==num:
+                return True
+            elif sq>num:
+                j=mid-1
+            else:
+                i=mid+1
+        else:
+            return False
+        
