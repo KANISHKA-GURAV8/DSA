3b90b664846040d92202f500d2e93e2f8723aeae
Kanishka Gurav <201110976+KANISHKA-GURAV8@users.noreply.github.com>
2026-09-13 20:38:03 +0530

    Time: 3 ms (54.81%) | Memory: 13.2 MB (53.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,9 +6,9 @@ class Solution(object):
         :rtype: List[int]
         """
         hash_map={}
-        for i in range(len(nums)):
+        for i in range(0,len(nums)):
             complement=target-nums[i]
             if complement in hash_map:
                 return [hash_map[complement],i]
+            
             hash_map[nums[i]]=i
-        return []
